21892086c8187818f6b02dd86f52cd0cd1683f0f
Aditya53yt <adityaverma989323@gmail.com>
2024-05-16 08:28:44 +0530

    first commit

---
+++ bulb.js
@@ -0,0 +1,13 @@
+ 
+ document.getElementById("img").src="bulb off.png"
+ 
+ 
+ function btnon(){
+   document.getElementById('img').src='bulb on.png';
+   document.getElementById('text').innerHTML="ON"
+}
+function btnof(){
+    document.getElementById('img').src='bulb off.png';
+    document.getElementById('text').innerHTML="OFF"
+
+}
